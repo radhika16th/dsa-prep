fbe1afa25820ebba36c6299d833ed7c7a16980da
Raadhikka Gupta <radhika16th@gmail.com>
2026-04-23 10:33:50 -0400

    insertion sort

---
+++ fundamentals/sorting/Insertion.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+/**
+ * Insertion Sort 
+ * 
+ * best: O(n) - already sorted
+ * worst: O(n^2)
+ * space: O(1)
+ */
+
+class Insertion {
+    // Array (shift elements)
+    public void insertionSortArray(int[] arr) {
+        for (int k = 1; k < arr.length - 1; k++) {
+            int current = arr[k];
+            int j = k;
+
+            while (j > 0 && arr[j - 1] > current) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+
+            arr[j] = current;
+        }
+    }
+
+    // ArrayList (shift elements)
+    public void insertionSortArrayList(ArrayList<Integer> list) {
+        for (int k = 1; k < list.size() - 1; k++) {
+            int current = list.get(k);
+            int j = k;
+
+            while (j > 0 && list.get(j - 1) > current) {
+                list.set(j, list.get(j - 1));
+                j--;
+            }
+
+            list.set(j, current);
+        }
+    }
+}
