6d1011ccb7a65306e407ac5757b05c9ed5fcf59b
Raadhikka Gupta <radhika16th@gmail.com>
2026-04-26 09:48:59 -0400

    bug fix

--- topics/linked-lists/HeadTailExample.java
+++ topics/linked-lists/HeadTailExample.java
@@ -12,8 +12,8 @@
  */
 
 public class HeadTailExample {
-    ListNode head = null;
-    ListNode tail = null;
+    private static ListNode head = null;
+    private static ListNode tail = null;
 
     // insert at end in O(1)
     public void insert(int val) {
@@ -29,24 +29,12 @@ public class HeadTailExample {
             tail = newNode; // move tail forward
         }
     }
-
-    // print
-    public void print() {
-        ListNode current = head;
-
-        while (current != null) {
-            System.out.print(current.value + " -> ");
-            current = current.next;
-        }
-
-        System.out.println("null");
-    }
-
+    
     public static void main(String[] args) {
         HeadTailExample list = new HeadTailExample();
         list.insert(1);
         list.insert(2);
         list.insert(3);
-        list.print();
+        LinkedBasics.print(head);
     }
 }
